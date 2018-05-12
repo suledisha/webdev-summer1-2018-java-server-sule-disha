1c49b3f2853eb0a313adcb861a035041296314ab
Disha <suledisha1@ccs.neu.edu>
2018-05-12 19:56:08 -0400

    Added User Service

--- src/main/java/com/example/myapp/repositories/UserRepository.java
+++ src/main/java/com/example/myapp/repositories/UserRepository.java
@@ -5,4 +5,3 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository
         extends CrudRepository<User, Integer> {}
-        
---
+++ src/main/java/com/example/myapp/services/UserService.java
@@ -0,0 +1,20 @@
+package com.example.myapp.services;
+
+import com.example.myapp.models.User;
+import com.example.myapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserService {
+    @Autowired
+    UserRepository repository;
+
+    @GetMapping("/api/user")
+    public List<User> findAllUsers() {
+        return (List<User>) repository.findAll();
+    }
+}
