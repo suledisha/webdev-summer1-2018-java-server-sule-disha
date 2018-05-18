368b25ccda471d51a337c42faad97ab82c4daec5
Disha <suledisha1@ccs.neu.edu>
2018-05-18 14:50:18 -0400

    Added Course Service

--- src/main/java/com/example/myapp/services/CourseService.java
+++ src/main/java/com/example/myapp/services/CourseService.java
@@ -3,10 +3,12 @@ package com.example.myapp.services;
 import com.example.myapp.models.Course;
 import com.example.myapp.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class CourseService {
     @Autowired
     CourseRepository courseRepository;
